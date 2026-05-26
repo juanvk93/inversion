a6d47be68aadba23aeba302edafc0bfc21930cb7
juanvk93 <juanvk93@gmail.com>
2026-05-26 14:26:25 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    - Pre-caché de assets locales en install
    - CDN externos (Chart.js, Google Fonts) se cachean bajo demanda */
 
-const CACHE = "cartera-v42";
+const CACHE = "cartera-v43";
 const ASSETS = [
   "./",
   "./index.html",
